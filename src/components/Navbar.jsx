bb6346a4ea62dbd8c6e04966d19d7f54d4956b7a
pamelagrhz <pamelagrhz@gmail.com>
2020-10-19 22:05:21 -0500

    Navbar creation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function NavBar() {
+  return (
+    <div className='nav-bar'>
+      <a href="mailto:contacto@innovacentre.com.mx">Contactanos</a>
+      <span>
+        <a href="http://google.com"> Registrarse
+      </a>
+        <span>/</span>
+        <a href="http://google.com"> Inicio de sesión
+      </a>
+      </span>
+    </div>
+  )
+}
