236aba63fa3f042dcf848d3261083c2fa02a9ac6
pamelagrhz <pamelagrhz@gmail.com>
2020-10-20 16:02:39 -0500

    Navbar redesign with bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap';
+
 
 export default function NavBar() {
   return (
-    <div className='nav-bar'>
-      <a href="mailto:contacto@innovacentre.com.mx">Contactanos</a>
-      <span>
-        <a href="http://google.com"> Registrarse
-      </a>
-        <span>/</span>
-        <a href="http://google.com"> Inicio de sesión
-      </a>
-      </span>
+    <div>
+      <div>
+        <Navbar className="primary" bg="primary" sticky="bottom" variant="dark">
+          <Navbar.Brand href="#home">Innova Centre</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="mailto:contacto@innovacentre.com.mx">Contactanos</Nav.Link>
+          </Nav>
+          <Form inline>
+            <Nav.Link href="/signout">Registrarse</Nav.Link>
+            <Nav.Link href="/signin">/ Iniciar sesión</Nav.Link>
+            <FormControl type="text" placeholder="Buscar" className="mr-sm-2" />
+            <Button variant="outline-light">Buscar</Button>
+          </Form>
+        </Navbar>
+        <br />
+      </div>
     </div>
+
   )
 }
