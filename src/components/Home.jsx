984fea2908027e6cd1236ce8101e2b155fccc4c5
pamelagrhz <pamelagrhz@gmail.com>
2020-11-19 22:50:43 -0600

    Modified home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,16 @@ import Logo from '../Assets/img/blue-logo.png'
 import Build from '../Assets/img/build.jpg'
 import IPad from '../Assets/img/ipad.jpg'
 import Desktop from '../Assets/img/desk.jpg'
+import RA from '../Assets/img/RA.png'
+import Blogs from '../Assets/img/blog-s.png'
+import Proces from '../Assets/img/proces.png'
+import Design from '../Assets/img/design.png'
+import Mark from '../Assets/img/mark.png'
+import MarkD from '../Assets/img/matk-dig.png'
+import Publicidad from '../Assets/img/publicidad.png'
+import CM from '../Assets/img/CM.png'
+import Ecom from '../Assets/img/ecom.png'
+import Blog from '../Assets/img/Blog.png'
 import Stars from './StyleComponent/Stars';
 import { faBolt, faKeyboard, faRocket, faDribble, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import WebPage from '../Assets/img/wbpg.png'
@@ -44,55 +54,118 @@ export default function Home() {
         </Parallax>
       </div>
       <div className="color-ch ">
-        <div className="app1">
-          <div className="med-size">
-            <img src={WebPage} alt="imagen" /></div>
-          <div className="med-size">Pagina web basica
-    <h6>A solo:</h6>
-            <h4>$XX.XX MXN</h4>
-            <Button variant="warning" size="lg">Más Información</Button></div>
+        <h3>Entra a nuestro blog</h3>
+        <p>Conoce más sobre noticias y nuestra comunidad. </p>
+        <img src={Blog} alt="" className="" />
+        <div>
+          <Button>Ir al blog</Button>
         </div>
       </div>
-      <Parallax speed={0}>
-        <div className="bg-card bg-red">
+
+      <div className="star-back">
+        <div class="bg-card bg-stars">
           <div className="app1">
             <div className="med-size">
-              <img src={WebPage} alt="imagen" /></div>
-            <div className="med-size">Pagina web basica
-    <h6>A solo:</h6>
+              <img src={RA} alt="imagen" /></div>
+            <div className="med-size">App de realidad aumentada
+              <h6>A solo:</h6>
               <h4>$XX.XX MXN</h4>
               <Button variant="warning" size="lg">Más Información</Button></div>
           </div>
         </div>
-      </Parallax>
-      <div className="app1">
-        {/* Servicios */}
-
-
-
-        <div className="med-size">
-          <Parallax speed={0}>
-            <img src={Desk} alt="" className="blue-logo" />
-          </Parallax>
+        <div class="bg-card bg-stars">
+          <div className="app1">
+            <div className="med-size">Pagina web basica
+              <h6>A solo:</h6>
+              <h4>$XX.XX MXN</h4>
+              <Button variant="warning" size="lg">Más Información</Button></div>
+            <div className="med-size">
+              <img src={WebPage} alt="imagen" /></div>
+          </div>
         </div>
-        <div className="med-size">
-          <Parallax speed={0}>
-            <p className=""> Implementación de ecommerce  </p>
-          </Parallax>
-          <Parallax speed={0}>
-            <p className="">Diseño gráfico  ​</p>
-          </Parallax>
-          <Parallax speed={0}>
-            <p className="">Marketing Digital</p>
-          </Parallax>
-          <Parallax speed={0}>
-            <p className="">Publicidad </p>
-          </Parallax>
-          <Parallax speed={0}>
-            <p className="">Community Manager </p>
-          </Parallax>
-          <br />
-          <Button variant="danger" size="lg">Boton  </Button>
+        <div class="bg-card bg-stars">
+          <div className="app1">
+            <div className="med-size">
+              <img src={Blogs} alt="imagen" /></div>
+            <div className="med-size">Consique tu blog
+              <h6>A solo:</h6>
+              <h4>$XX.XX MXN</h4>
+              <Button variant="warning" size="lg">Más Información</Button></div>
+          </div>
+        </div>
+        <div class="bg-card bg-stars">
+          <div className="app1">
+            <div className="med-size">Servicio de diseño gráfico
+              <h6>A solo:</h6>
+              <h4>$XX.XX MXN</h4>
+              <Button variant="warning" size="lg">Más Información</Button></div>
+            <div className="med-size">
+              <img src={Design} alt="imagen" /></div>
+          </div>
+        </div>
+        <div class="bg-card bg-stars">
+          <div className="app1">
+
+            <div className="med-size">
+              <img src={Proces} alt="imagen" /></div>
+            <div className="med-size">Consultoría de procesos
+              <h6>A solo:</h6>
+              <h4>$XX.XX MXN</h4>
+              <Button variant="warning" size="lg">Más Información</Button></div>
+          </div>
+        </div>
+        <div class="bg-card bg-stars">
+          <div className="app1">
+            <div className="med-size">Marketing
+              <h6>A solo:</h6>
+              <h4>$XX.XX MXN</h4>
+              <Button variant="warning" size="lg">Más Información</Button></div>
+            <div className="med-size">
+              <img src={Mark} alt="imagen" /></div>
+          </div>
+        </div>
+        <div class="bg-card bg-stars">
+          <div className="app1">
+
+            <div className="med-size">
+              <img src={MarkD} alt="imagen" /></div>
+            <div className="med-size">Marketing Digital
+              <h6>A solo:</h6>
+              <h4>$XX.XX MXN</h4>
+              <Button variant="warning" size="lg">Más Información</Button></div>
+          </div>
+        </div>
+        <div class="bg-card bg-stars">
+          <div className="app1">
+            <Stars></Stars>
+            <div className="med-size">Publicidad
+              <h6>A solo:</h6>
+              <h4>$XX.XX MXN</h4>
+              <Button variant="warning" size="lg">Más Información</Button></div>
+            <div className="med-size">
+              <img src={Publicidad} alt="imagen" /></div>
+          </div>
+        </div>
+        <div class="bg-card bg-stars">
+          <div className="app1">
+
+            <div className="med-size">
+              <img src={CM} alt="imagen" /></div>
+            <div className="med-size">CCommunity Manager
+              <h6>A solo:</h6>
+              <h4>$XX.XX MXN</h4>
+              <Button variant="warning" size="lg">Más Información</Button></div>
+          </div>
+        </div>
+        <div class="bg-card bg-stars">
+          <div className="app1">
+            <div className="med-size">Implementación de ecommerce
+              <h6>A solo:</h6>
+              <h4>$XX.XX MXN</h4>
+              <Button variant="warning" size="lg">Más Información</Button></div>
+            <div className="med-size">
+              <img src={Ecom} alt="imagen" /></div>
+          </div>
         </div>
       </div>
 
